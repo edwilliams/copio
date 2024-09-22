aa095cb70948fb9b2c5642d1f25ccb0a78b4ed57
Ed Williams <ed.williams@stepstone.com>
2024-09-22 07:27:25 +0100

    remove legacy new version button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 // or `BrowserRouter as Router` to use popstate
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Provider from 'store'
-
-import { removeServiceWorkers } from 'utils/general'
-import Button from 'components/elements/Button'
-
 import Home from 'containers/home'
 import Add from 'containers/add'
 import Edit from 'containers/edit'
@@ -15,50 +11,21 @@ import { MuiThemeProvider } from '@material-ui/core/styles'
 
 import theme from 'theme'
 
-import polyfills from 'lib/polyfills'
-
-polyfills()
-
-const App = () => {
-  const [showInstallBtn, setShowInstallBtn] = useState(false)
-
-  const clickInstallNewVersion = async () => {
-    await removeServiceWorkers()
-    window.location.reload()
-  }
-
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Button
-        onClick={clickInstallNewVersion}
-        color="primary"
-        style={{
-          display: showInstallBtn ? 'block' : 'none',
-          position: 'absolute',
-          zIndex: '999',
-          background: 'red',
-          bottom: '0',
-          left: '0',
-          margin: '15px',
-          color: 'white',
-        }}
-      >
-        New version available
-      </Button>
-      <Provider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/add" component={Add} />
-            <Route path="/edit/:id_song" component={Edit} />
-            <Route path="/login" component={Home} />
-            <Route path="/reset" component={Home} />
-            <Route path="/songs/:id_song" component={Song} />
-          </Switch>
-        </Router>
-      </Provider>
-    </MuiThemeProvider>
-  )
-}
+const App = () => (
+  <MuiThemeProvider theme={theme}>
+    <Provider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/add" component={Add} />
+          <Route path="/edit/:id_song" component={Edit} />
+          <Route path="/login" component={Home} />
+          <Route path="/reset" component={Home} />
+          <Route path="/songs/:id_song" component={Song} />
+        </Switch>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>
+)
 
 export default App
